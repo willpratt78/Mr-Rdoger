38457048e8af3629f0c6009bb06dd96587bdc68f
William Pratt <willpratt78@gmail.com>
2022-04-01 09:43:01 -0700

    function replaces the number 3 as directed

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 let base = 0
 const array = [5]
 let newArray =[]
+let returnArray = []
 function numberArray() {
-  let base = 0
-const array = [5]
-let newArray =[]
   while (array >= base) {
   newArray.push(base ++)
 }
 }
 console.log(newArray)
 
+function numberReplacement() {
+newArray.forEach(function(element) {
+    if  (element === 3) {
+       returnArray.push("Won't you be my neighbor?");
+    }
+    else {
+      returnArray.push(element);
+    };
+  });
+};
