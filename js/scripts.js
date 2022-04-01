d4bce0cf6c100e14e19cced2860e653341b0d322
William Pratt <willpratt78@gmail.com>
2022-04-01 10:25:19 -0700

    go out of order for TDD, 1 and 2 return intended results

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let base = 0
-const array = [5]
+const array = [20]
 let newArray =[]
 let returnArray = []
 function numberArray() {
@@ -14,8 +14,22 @@ newArray.forEach(function(element) {
     if  (element === 3) {
        returnArray.push("Won't you be my neighbor?");
     }
+    else if (element === 2) {
+      returnArray.push("Boop!");
+    }
+    else if (element === 1) {
+      returnArray.push("Beep!");
+    }
     else {
       returnArray.push(element);
     };
   });
 };
+
+
+
+numberArray(array)
+console.log(newArray)
+
+numberReplacement(newArray)
+console.log(returnArray)
